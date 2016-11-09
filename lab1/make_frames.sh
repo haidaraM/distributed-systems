3732d227fbb60fc07d5c4bb5350fecf7c6720ad7
elmhaidara <elmhaidara@gmail.com>
2016-11-09 18:02:24 +0100

    [lab1] update bootstrap and make_frames

--- lab1/make_frames.sh
+++ lab1/make_frames.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "You must specify the port number"
+    exit 1
+fi
+
+port=$1
 echo "<html><body>"
 for y in `cat neighborlist.txt` 
 do 
-echo "<iframe src=\"http://$y\" width="48%" height=300></iframe>"
+echo "<iframe src=\"http://$y:${port}\" width="48%" height=300></iframe>"
 done
 echo "</body></html>"
