04a3c2a48714dca36fb73e825fa4869e144ddcff
elmhaidara <elmhaidara@gmail.com>
2016-12-08 22:58:40 +0100

    [lab3] Updated the workers to delete the added posts

--- lab3/workers.py
+++ lab3/workers.py
@@ -2,15 +2,20 @@ import Queue
 import random
 import subprocess
 import threading
-import time
 
 # number max of messages to send to all the vessels
-NB_MAX_MESSAGE = 20
+NB_MAX_MESSAGE = 15
 
 # the seattle port number
 DEFAULT_PORT = 63159
 
 NEIGHBORS_FILE_NAME = "neighborlist.txt"
+
+# number of delete messages sent
+delete_counter = 0
+
+# lock used to count the number of delete messages sent
+delete_counter_lock = threading.Lock()
 
 
 class PostThread(threading.Thread):
@@ -35,8 +40,8 @@ class PostThread(threading.Thread):
         for i in range(self.nb_messages):
             waiting_time = random.randint(1, 3)
             # print "Waiting %d seconds before sending a post to %s... " % (waiting_time, self.url)
-            time.sleep(waiting_time)
-            args = ['curl', '--silent', '-d', 'entry=curl' + str(i), '-X', 'POST', self.url]
+            # time.sleep(waiting_time)
+            args = ['curl', '--silent', '-d', 'entry=' + str(i) + '_from_' + self.target_ip, '-X', 'POST', self.url]
 
             res = subprocess.Popen(args=args, stdout=subprocess.PIPE)
             output, _ = res.communicate()
@@ -44,9 +49,9 @@ class PostThread(threading.Thread):
                 output = output.strip().split('_')
                 entry_id = output[0]
                 entry_creator_ip = output[1]
-                # print "Entry id : ", entry_id, ". Entry creator : ", entry_creator_ip
 
                 if self.delete_queue is not None:
+                    # print "Adding entry %d_%s in the queue" % (int(entry_id), entry_creator_ip)
                     self.delete_queue.put((entry_id, entry_creator_ip))
 
 
@@ -63,17 +68,29 @@ class DeleteThread(threading.Thread):
         self.url = "http://{0}:{1}/entries".format(self.target_ip, self.port_number)
 
     def run(self):
-        while True:
-            time.sleep(random.randint(1, 3))
-            entry_id, entry_creator_ip = self.delete_queue.get()
-            print "Deleting the entry : %d_%s" % (int(entry_id), entry_creator_ip)
+        global delete_counter
 
-            args = ['curl', '--silent', '-d', 'delete=1', '-d', 'creator_ip=' + entry_creator_ip, '-X', 'POST',
-                    self.url + "/" + str(entry_id)]
+        while delete_counter < max_delete:
+            try:
+                entry_id, entry_creator_ip = self.delete_queue.get_nowait()
+                print "Deleting the entry : %d_%s by %s" % (int(entry_id), entry_creator_ip, self.target_ip)
 
-            subprocess.Popen(args=args, stdout=subprocess.PIPE)
+                args = ['curl', '--silent', '-d', 'delete=1', '-d', 'creator_ip=' + entry_creator_ip, '-X', 'POST',
+                        self.url + "/" + str(entry_id)]
 
-            self.delete_queue.task_done()
+                subprocess.Popen(args=args, stdout=subprocess.PIPE)
+
+                delete_counter_lock.acquire()
+
+                delete_counter += 1
+
+                # print 'Counter :', delete_counter
+
+                delete_counter_lock.release()
+
+                self.delete_queue.task_done()
+            except Queue.Empty:
+                pass
 
 
 def _read_neighborlist():
@@ -95,6 +112,10 @@ if __name__ == '__main__':
 
     nb_message_per_vessel = int(NB_MAX_MESSAGE / nb_vessel)
 
+    # we compute the real number of delete message
+    global max_delete
+    max_delete = nb_message_per_vessel * nb_vessel
+
     # Delete messages queues
     q = Queue.LifoQueue()
 
@@ -106,10 +127,8 @@ if __name__ == '__main__':
         post_thread = PostThread(vessel_ip, DEFAULT_PORT, nb_message_per_vessel, q)
         post_thread.start()
 
+    delete_threads = []
     # launch deleting thread
     for vessel_ip in reversed(neighbortlist):
         delete_thread = DeleteThread(vessel_ip, DEFAULT_PORT, q)
-        delete_thread.setDaemon(True)
         delete_thread.start()
-
-    q.join()
